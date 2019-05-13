037e72585f73f6637478badf780c2e35ccdd4000
valkyrienyanko <sebastianbelle074@gmail.com>
2019-05-13 18:54:14 -0500

    Enemy Hit

--- LightNPCs.cs
+++ LightNPCs.cs
@@ -7,13 +7,28 @@ namespace LightingEssentials
 {
     class LightNPCs : GlobalNPC
     {
+        float red = 0f;
+
+        public override bool InstancePerEntity
+        {
+            get
+            {
+                return true;
+            }
+        }
+
         public override void DrawEffects(NPC npc, ref Color drawColor)
         {
-            if (npc.type == NPCID.EyeofCthulhu) {
-                Lighting.AddLight(npc.position, new Vector3(1.0f, 0, 0));
-            } else if (!npc.townNPC && !npc.friendly) {
-                Lighting.AddLight(npc.position, new Vector3(0.3f, 0, 0));
+            if (red > 0.0f)
+            {
+                Lighting.AddLight(npc.position, new Vector3(red, 0, 0));
+                red -= 0.01f;
             }
+        }
+
+        public override void HitEffect(NPC npc, int hitDirection, double damage) {
+            float ratio = (float) npc.life / npc.lifeMax;
+            red = (ratio - 1) * -1;
         }
     }
 }
